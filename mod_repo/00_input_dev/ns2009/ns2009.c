ac50b85f0ddc6aef3bb691db9f1836d449ba4936
hz2 <writeforever@foxmail.com>
2021-08-02 09:10:00 +0800

    daily udpate.

--- mod_repo/00_input_dev/ns2009/ns2009.c
+++ mod_repo/00_input_dev/ns2009/ns2009.c
@@ -13,49 +13,60 @@
 #include <linux/i2c.h>
 #include <linux/regmap.h>
 
-static const struct input_id ns2009_input_id = {
-	.bustype = BUS_I2C,
-	.product = 2009,
-};
-
-static int ns2009_cmd(struct device *dev, u8 cmd)
-{
-	return 0;
-}
-
-static int ns2009_probe(struct i2c_client *i2c,
+static int ns2009_probe(struct i2c_client *client,
 			const struct i2c_device_id *id)
 {
 	return 0;
 }
 
-static int ns2009_remove(struct spi_device *spi)
+static int ns2009_remove(struct i2c_client *client)
 {
 	return 0;
 }
 
-static const struct i2c_device_id ns2009_idtable[] = {
-	{ "ns2009", "0" },
-	{ }
-};
 
 static const struct of_device_id ns2009_of_match[] = {
 	{ .compatible = "ns2009" },
 	{ }
 };
-MODULE_DEVICE_TABLE(of, ns2009_of_match);
 
 static struct i2c_driver ns2009_driver = {
 	.driver = {
 		.name = "ns2009",
 		.of_match_table = of_match_ptr(ns2009_of_match),
+		/* .pm   = *ns2009_pm_ops, */
 	},
-	.id_table     = ns2009_idtable,
+
+	//.id_table     = ns2009_idtable,
 	.probe        = ns2009_probe,
 	.remove       = ns2009_remove,
+
+	/* if device autodetection is needed: */
+	/*
+	 * .class	    = I2C_CLASS_SOMETHING,
+	 * .detect	    = ns2009_detect,
+	 * .address_list    = normal_i2c,
+	 *
+	 * .shutdown	    = ns2009_shutdown, //optional
+	 * .command	    = ns2009_command,  //optional, deprecated 
+	 * */
 };
+
+/*static int __init ns2009_init(void)
+{
+	return i2c_add_driver(&ns2009_driver);
+}
+module_init(ns2009_init);
+
+static void __exit ns2009_exit(void)
+{
+	return i2c_del_driver(&ns2009_driver);
+}
+module_exit(ns2009_exit);
+*/
 module_i2c_driver(ns2009_driver);
 
 MODULE_AUTHOR("Zheng Hua <writeforever@foxmail.com>");
+MODULE_ALIAS("ns2009-i2c");
 MODULE_DESCRIPTION("NS2009 Touchscreen Driver");
 MODULE_LICENSE("GPL");
---
+++ mod_repo/00_input_dev/ns2009/ns2009.mod
@@ -0,0 +1,2 @@
+/home/yuanxin/source/applications/mod_repo/00_input_dev/ns2009/ns2009.o
+
