6e09ebf79bbbe0cc15080869153eb9e177c84f0a
huazheng <writeforever@foxmail.com>
2022-02-27 13:20:53 -0500

    add applications/i2c-smbus/at24cxx
    Signed-off-by: huazheng <writeforever@foxmail.com>

---
+++ applications/i2c-smbus/at24cxx/include/at24cxx.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifndef __AT24CXX_H
+#define __AT24CXX_H
+
+class at24cxx{
+
+private:
+protected:
+public:
+
+
+};
+
+#endif
---
+++ applications/i2c-smbus/at24cxx/main.c
@@ -0,0 +1,10 @@
+#include <i2c/smbus.h>
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    
+
+    printf("Hello, world!\n");
+    return 0;
+}
