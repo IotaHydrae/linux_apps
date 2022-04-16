89fb0691ce0491a7b77ebfcc466c48a4f223cc11
IotaHydrae <writeforever@foxmail.com>
2022-04-16 12:52:49 +0800

    update
    Signed-off-by: IotaHydrae <writeforever@foxmail.com>

--- drivers/i2c_drivers/ADS1015/ads1015.c
+++ drivers/i2c_drivers/ADS1015/ads1015.c
@@ -1,43 +1,100 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/device.h>
+#include <linux/capability.h>
+#include <linux/jiffies.h>
+#include <linux/i2c.h>
+#include <linux/mutex.h>
 
-struct ads1015_data {
-	struct i2c_client *client;
-	
+/* ADS101X operating modes */
+enum {
+	ADS101X_MODE_SINGLE_SHOT,
+	ADS101X_MODE_CONTINUOUS_CONVERSION,
 };
 
-static int 
-ads1015_probe(struct i2c_client *client, const struct i2c_device_id *id)
-{
-	struct ads1015_data *ads1015;
+struct ads101x_regs {
+	u8 conversion;
+	u8 config;
+	u8 lo_thresh;
+	u8 hi_thresh;
+}
 
-	ads1015 = devm_kzalloc(&client->dev, sizeof(struct ads1015_data),
+struct ads101x_regs ads101x_regs_ads1015 = {
+	.conversion = 0x00,
+	.config     = 0x01,
+	.lo_thresh  = 0x02,
+	.hi_thresh  = 0x03,
+}
+
+struct ads101x_data {
+	struct i2c_client *client;
+	struct ads101x
+
+	struct work_struct work;
+	wait_queue_head_t wait_queue;
+	spinlock_t lock;	
+};
+
+static void ads101x_work_handler(struct work_struct *work)
+{
+	struct ads101x_data *ads101x = container_of(work, \
+						struct ads101x_data, work);
+	/* do some work */
+}
+
+static int 
+ads101x_probe(struct i2c_client *client, const struct i2c_device_id *id)
+{
+	struct ads101x_data *ads101x;
+
+	ads101x = devm_kzalloc(&client->dev, sizeof(struct ads101x_data),
 			GFP_KERNEL);
-	if(!ads1015)
+	if(!ads101x)
 		return -ENOMEM;
 
-	ads1015->client = client;
-	i2c_set_clientdata(client, ads1015);
+	INIT_WORK(&ads101x->work, )
+	init_waitqueue_head(&ads101x->wait_queue);
+	spin_lock_init(&ads101x->lock);
+	
+	ads101x->client = client;
+	i2c_set_clientdata(client, ads101x);
 
 	return 0;
 }
 
 static int
-ads1015_remove(struct i2c_client *client)
+ads101x_remove(struct i2c_client *client)
 {
+	struct ads101x_data *ads101x = i2c_get_clientdata(client);
+
+	kfree(ads101x);
 	return 0;
 }
 
-static const struct of_device_id ads1015_of_match_table[] = {
-	{.compatible = "ti,ads1015"},
+static struct of_device_id ads101x_of_match_table[] = {
+	{ .compatible = "ti,ads101x" },
 	{},
-}
-
-static struct i2c_driver ads1015_i2c_driver = {
-	.probe = ads1015_probe,
-	.remove = ads1015_remove, 	
 };
+MODULE_DEVICE_TABLE(of, ads101x_of_match_table);
+
+static struct i2c_device_id ads101x_id_table[] = {
+	{ "ads101x", 0 },
+	{},
+};
+MODULE_DEVICE_TABLE(i2c, ads101x_id_table);
+
+static struct i2c_driver ads101x_i2c_driver = {
+	.probe = ads101x_probe,
+	.remove = ads101x_remove,
+	.driver = {
+		.name = "ads101x",
+		.of_match_table = of_match_ptr(ads101x_of_match_table),
+	},
+
+	.id_table = ads101x_id_table,
+};
+module_i2c_driver(ads101x_i2c_driver);
 
 MODULE_AUTHOR("IotaHydrae writeforever@foxmail.com");
-MODULE_DESCRIPTION("a i2c driver for ads1015");
+MODULE_DESCRIPTION("a i2c driver for ads101x");
 MODULE_LICENSE("GPL");
