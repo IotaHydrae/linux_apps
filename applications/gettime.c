464434fa947d7490edc05ed15c7295d8867c29b3
IotaHydrae <writeforever@foxmail.com>
2022-04-21 15:36:27 +0800

    update

---
+++ applications/gettime.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <time.h>
+
+int
+main(int argc, char **argv)
+{
+    struct timeval tv;
+    struct tm tm_res;
+
+    while(1){
+        gettimeofday(&tv, NULL);
+	localtime_r(&tv.tv_sec, &tm_res);
+        printf("%d:%d:%d\n",tm_res.tm_hour, tm_res.tm_min, tm_res.tm_sec);
+        sleep(1);
+    }
+
+}
